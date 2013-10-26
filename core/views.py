104d1f76d811b46640470e447be8b7e13dfca7e2
fredsilva <fredsilva.sistemas@gmail.com>
2013-10-26 21:56:04 -0200

    Consultas e paginação

--- core/views.py
+++ core/views.py
@@ -2,12 +2,28 @@
 from django.shortcuts import render, get_object_or_404
 from models import Post
 from forms import CommentForm
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     posts = Post.objects.all()
-    content = {}
-    content['posts'] = posts
-    return render(request, 'index.html', content)
+    search = request.GET.get('search','')
+    if search:
+        posts = posts.filter(Q(title__icontains=search) | Q(content__icontains=search))
+    #paginação
+    paginator = Paginator(posts,3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    #fim da paginação
+    context = {}
+    context['posts'] = posts
+    context['search'] = search
+    return render(request, 'index.html', context)
 
 def post_detail(request, pk):
     queryset = Post.objects.all()
