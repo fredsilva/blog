6f793831802cb00f4d8913beab9ca373911da0e7
fredsilva <fredsilva.sistemas@gmail.com>
2013-10-26 20:39:52 -0200

    Details

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import Post
 
 def home(request):
@@ -7,3 +7,10 @@ def home(request):
     content = {}
     content['posts'] = posts
     return render(request, 'index.html', content)
+
+def post_detail(request, pk):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, pk=pk)
+    context = {}
+    context['post'] = post
+    return render(request, 'post_detail.html', context)
--- urls.py
+++ urls.py
@@ -5,9 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'Blog.core.views.home', name='home'),
+    url(r'^post/(?P<pk>\d+)/$', 'Blog.core.views.post_detail', name='post_detail'),
     # url(r'^Blog/', include('Blog.foo.urls')),
-
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
